479a54251ee1d0c92686440d9b1d9b821656ce92
mmythily <mythily.mudunuru@gmail.com>
2019-07-09 15:20:54 -0400

    add 3 stores ex1

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+
+# Exercise 5: Calculations
+# Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
