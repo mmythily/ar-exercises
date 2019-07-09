1e55c23c7a593fb013cac2ab749759e5545d72cc
mmythily <mythily.mudunuru@gmail.com>
2019-07-09 17:13:02 -0400

    calculate sum, average, count and chain methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -12,7 +12,10 @@ puts "----------"
 
 # Exercise 5: Calculations
 # Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
-puts Store.sum(:annual_revenue)
+puts "Total sum of annual revenue #{Store.sum(:annual_revenue)}"
 # On the next line, also output the average annual revenue for all stores.
-# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
+puts "Average of annual revenue #{Store.average(:annual_revenue)}"
 
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
+@num_of_rich_stores = Store.where("annual_revenue > 1000000").count()
+puts "Number of stores making more than $10000000 : #{@num_of_rich_stores}"
