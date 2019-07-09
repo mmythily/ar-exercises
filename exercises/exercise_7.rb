16b56ae4e8649f7ce5f6f2e9e4011b0f60c15f9a
mmythily <mythily.mudunuru@gmail.com>
2019-07-09 18:13:05 -0400

    validation code in progress, still needs testing

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -14,12 +14,26 @@ puts "----------"
 
 # Exercise 7: Validations for both models
 # Add validations to two models to enforce the following business rules:
-# Employees must always have a first name present
-# Employees must always have a last name present
-# Employees have a hourly_rate that is a number (integer) between 40 and 200
-# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
-# Stores must always have a name that is a minimum of 3 characters
-# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+class Employee < ActiveRecord
+    # Employees must always have a first name present
+    validates :first_name, presence: true
+    # Employees must always have a last name present
+    validates :last_name, presence: true
+    # Employees have a hourly_rate that is a number (integer) between 40 and 200
+    validates :hourly_rate, numericality: {greater_than: 40, less_than:200}
+    # Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+    belongs_to :store
+    validates :store, presence: true
+    
+end
+
+class Store < ActiveRecord
+    # Stores must always have a name that is a minimum of 3 characters
+    validates :name, length: {minimum: 3}
+    # Stores have an annual_revenue that is a number (integer) that must be 0 or more
+
+end
+
 # BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
 # Ask the user for a store name (store it in a variable)
 # Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
