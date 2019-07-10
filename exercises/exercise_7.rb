44d8bffd880909867ff54d6005e6965130b7c0b7
mmythily <mythily.mudunuru@gmail.com>
2019-07-09 20:41:02 -0400

    validation test:

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -11,30 +11,12 @@ puts "----------"
 
 # Your code goes here ...
 
-
-# Exercise 7: Validations for both models
-# Add validations to two models to enforce the following business rules:
-class Employee < ActiveRecord
-    # Employees must always have a first name present
-    validates :first_name, presence: true
-    # Employees must always have a last name present
-    validates :last_name, presence: true
-    # Employees have a hourly_rate that is a number (integer) between 40 and 200
-    validates :hourly_rate, numericality: {greater_than: 40, less_than:200}
-    # Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
-    belongs_to :store
-    validates :store, presence: true
-    
-end
-
-class Store < ActiveRecord
-    # Stores must always have a name that is a minimum of 3 characters
-    validates :name, length: {minimum: 3}
-    # Stores have an annual_revenue that is a number (integer) that must be 0 or more
-
-end
-
-# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
 # Ask the user for a store name (store it in a variable)
 # Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
 # Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+puts "What's the store name?"
+@store_name = gets.chomp
+@store_new = Store.new(name: @store_name)
+puts @store_new.valid?
+@store_new.errors.messages.each {|key,message| puts message}
+@store_new.save
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
 class Store < ActiveRecord::Base
     has_many :employees
+
+    # Stores must always have a name that is a minimum of 3 characters
+    validates :name, length: {minimum: 3}
+    # Stores have an annual_revenue that is a number (integer) that must be 0 or more
+    validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+
+    # BONUS: Stores must carry at least one of the men's or women's apparel 
+    #(hint: use a custom validation method - don't use a Validator class)
+    validate :apparel_both_gender
+
+    def apparel_both_gender
+        mens_apparel == true or womens_apparel == true
+    end
+
 end
